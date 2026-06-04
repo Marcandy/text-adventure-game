85273f6f74438c3b0856b8d7a1c943eeb9f66141
Carolista <cjones.renaissancewoman@gmail.com>
2026-06-04 13:24:27 -0400

    refactored with TODOs in OldMan and Shopkeeper classes

---
+++ starter-code/classes/characters/OldMan.js
@@ -0,0 +1,19 @@
+/**** OLD MAN CLASS ****/
+
+import Character from "./Character.js";
+
+/*
+   Inherits from Character.
+   Serves specific purpose when interacting with hero.
+   May or may not have items or coins for the hero.
+   Initial instance will be to provide Link with a sword.
+*/
+
+// TODO: Inherit from Character
+export default class OldMan {
+
+	// TODO: Define constructor
+
+	// TODO: Override speak()
+    
+}
---
+++ starter-code/classes/characters/Shopkeeper.js
@@ -0,0 +1,20 @@
+/**** SHOPKEEPER CLASS ****/
+
+import { log } from "../../utils/logger.js";
+import Character from "./Character.js";
+
+/*
+   Inherits from Character.
+   Offers items for sale.
+*/
+
+// TODO: Inherit from Character
+export default class Shopkeeper {
+
+	// TODO: Define constructor
+
+	// TODO: Override speak()
+
+	// TODO: Define method that displays items for sale
+    
+}
