74dc264a31cf30b3c17f8549c978878968410808
Carolista <cjones.renaissancewoman@gmail.com>
2026-06-04 06:36:18 -0400

    added main sequence

--- solution/main.js
+++ solution/main.js
@@ -1,1 +1,26 @@
-console.log("Testing 1 2 3...");
+/**** MAIN EXECUTION CODE ****/
+
+import Game from "./models/Game.js";
+
+/*
+   This simulation of the opening sequence of the classic 1986 NES game, 
+   The Legend of Zelda, should give you an idea of how to think about 
+   building an application modularly with classes and helper functions.
+*/
+
+function runProgram() {
+	// Instantiate our unified modular controller engine
+	const ZeldaDemo = new Game();
+
+	// Run introduction text panel sequence
+	ZeldaDemo.displayIntroduction();
+
+	// CORE GAME INTERACTION LOOP: Keeps turning until conditions flags drop
+	while (!ZeldaDemo.isGameOver) {
+		ZeldaDemo.playNextTurn();
+	}
+
+	console.log("\n--- Thanks for playing! ---");
+}
+
+runProgram();
